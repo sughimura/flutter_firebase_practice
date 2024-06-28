bdc71cb68f0fd359dd538b65a1ef1dbef26a0d1d
sughimura <nxsughi@gmail.com>
2024-06-28 21:49:21 +0900

    feat: add firestore '実行' ボタン

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,6 +43,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(24),
@@ -112,6 +116,19 @@ class _MyHomePageState extends State<MyHomePage> {
                       print(e);
                     }
                   }),
+              TextButton(
+                // ボタンを押した時のイベント
+                onPressed: () {
+                  /* ここにプログラムを記載 */
+                  FirebaseFirestore.instance
+                      .doc('autoCollection1/autoDocument1')
+                      .delete();
+                },
+                child: const Text(
+                  '実行',
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
             ],
           ),
         ),
