56600aaa91049168111dc638f50e9b50810e1b81
sughimura <nxsughi@gmail.com>
2024-06-29 16:37:43 +0900

    refactor: move to page/my_home_page.dart

--- lib/service/router.dart
+++ lib/service/router.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
 import '../ui/page/details_screen.dart';
-import '../my_home_page.dart';
+import '../ui/page/my_home_page.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
