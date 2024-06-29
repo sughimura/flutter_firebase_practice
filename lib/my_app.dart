097198b10f9bcf86bb74c0baba21bec12c4a4b4c
sughimura <nxsughi@gmail.com>
2024-06-29 15:15:33 +0900

    refactor: extract my_home_page.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+import 'my_home_page.dart';
 
 final GoRouter _router = GoRouter(
   routes: <RouteBase>[
@@ -29,131 +29,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: _router,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // 入力したメールアドレス・パスワード
-  String _email = '';
-  String _password = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // 1行目 メールアドレス入力用テキストフィールド
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'メールアドレス'),
-                onChanged: (String value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-              ),
-              // 2行目 パスワード入力用テキストフィールド
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'パスワード'),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    _password = value;
-                  });
-                },
-              ),
-              // 3行目 ユーザ登録ボタン
-              ElevatedButton(
-                child: const Text('ユーザ登録'),
-                onPressed: () async {
-                  try {
-                    final User? user = (await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                        email: _email, password: _password))
-                        .user;
-                    if (user != null)
-                      print("ユーザ登録しました ${user.email} , ${user.uid}");
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-              // 4行目 ログインボタン
-              ElevatedButton(
-                child: const Text('ログイン'),
-                onPressed: () async {
-                  try {
-                    // メール/パスワードでログイン
-                    final User? user = (await FirebaseAuth.instance
-                        .signInWithEmailAndPassword(
-                        email: _email, password: _password))
-                        .user;
-                    if (user != null)
-                      print("ログインしました　${user.email} , ${user.uid}");
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-              // 5行目 パスワードリセット登録ボタン
-              ElevatedButton(
-                  child: const Text('パスワードリセット'),
-                  onPressed: () async {
-                    try {
-                      await FirebaseAuth.instance
-                          .sendPasswordResetEmail(email: _email);
-                      print("パスワードリセット用のメールを送信しました");
-                    } catch (e) {
-                      print(e);
-                    }
-                  }),
-              TextButton(
-                // ボタンを押した時のイベント
-                onPressed: () {
-                  /* ここにプログラムを記載 */
-                  FirebaseFirestore.instance
-                      .collection('flutterDataCollection')
-                      .doc('flutterDataDocument')
-                      .get()
-                      .then((ref) {
-                    print(ref.get('mydata'));
-                  });
-                },
-                child: const Text(
-                  '取得',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-              TextButton(onPressed: () {
-                FirebaseFirestore.instance
-                    .collection('flutterDataCollection')
-                    .doc('flutterDataDocument')
-                    .set({'autofield': "abc"}, SetOptions(merge: true));
-              }, child: const Text('登録', style: TextStyle(fontSize: 30),)
-              ),
-              ElevatedButton(
-                onPressed: () => context.go('/details'),
-                child: const Text('Go to the Details screen'),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
