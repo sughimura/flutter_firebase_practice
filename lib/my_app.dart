cab15d19c9c51f4cd2781e46be7b5b37848d92dc
sughimura <nxsughi@gmail.com>
2024-06-29 15:17:14 +0900

    refactor: extract details_screen.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'details_screen.dart';
 import 'my_home_page.dart';
 
 final GoRouter _router = GoRouter(
@@ -29,25 +30,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: _router,
-    );
-  }
-}
-
-/// The details screen
-class DetailsScreen extends StatelessWidget {
-  /// Constructs a [DetailsScreen]
-  const DetailsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Details Screen')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.go('/'),
-          child: const Text('Go back to the Home screen'),
-        ),
-      ),
     );
   }
 }
